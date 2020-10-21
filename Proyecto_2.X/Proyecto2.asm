dca047900fcfa0181cdfd40a8279f50285a0a279
mun19508 <68123854+mun19508@users.noreply.github.com>
2020-10-21 00:23:07 -0600

    v3
    Agregar lo que se muestran en los comentarios y además agregar una manera de como enviar los datos

--- Proyecto_2.X/Proyecto2.asm
+++ Proyecto_2.X/Proyecto2.asm
@@ -14,8 +14,10 @@
 ; CONFIG2
 ; __config 0xFFFF
  __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
- 
- 
+;-----------------------------------Constantes------------------------------------------- 
+VALOR_CERO	EQU	.0
+DISPLAYS_X	EQU	.1	
+;-------------------------------------Variables------------------------------------------	
 PR_VAR		UDATA
 CONT1		    RES 1		    ;Variable para delays
 CONT2		    RES 1		    ;para delays
@@ -26,7 +28,7 @@ DISY_L		    RES 1		    ;contiene los bits 0 - 99, eje y
 DISY_H	    RES 1		    ;contiene los valores de  0 - 9, eje y
 NIBB_L		    RES 1		    ;utilizada para los nibbles menos significativos
 NIBB_H		    RES 1		    ;utilizada para el nibble mas significativo
-LED		    RES 1		    ;variable para mostrar el valor de los leds
+LED		    RES 1		    ;variable para selecionar el eje actual.
 TEMP_STATUS	    RES 1		    ;para interrupcion
 TEMP_W	    RES 1		    ;para	    ||
 FLAGS		    RES 1		    ;para el multiplexeo  
@@ -42,11 +44,19 @@ SAVE:					;Sirve para guardar el valor actual de:
     MOVWF	TEMP_STATUS		;& STATUS
 ISR:
     BTFSS	PIR1, ADIF
-    GOTO		LOAD
+    GOTO		ADC_C
+    BTFSS	PIR1, RCIF		
+    GOTO		RECEPCION
+ADC_C:
+    ;Configurar este apartado para cambiar de canal: ADCON, 2 =1 -> AN9 & ADCON, 2 =1 -> AN8
     BCF		PIR1, ADIF
     MOVFW	ADRESH
     MOVWF	DISPLAY_V
     BSF		ADCON0, GO
+   GOTO		LOAD
+RECEPCION:
+    
+    GOTO		LOAD
 LOAD:					;Se recupera el valor de:
     SWAPF	TEMP_STATUS, W
     MOVWF	STATUS		;STATUS
@@ -88,35 +98,59 @@ START
     BCF		ADCON1, ADFM		;Justificado a la izquierda.
     BCF		ADCON1, VCFG1		;Ref: VSS.
     BCF		ADCON1, VCFG0		;Ref: VDD.
+     ;-----------------Configuracion de Recepcion/transmision-----------------------------
+    MOVWF	SPBRG			;Baud Rate 9615.
+    BSF		TXSTA, 2		;Modo de alta velocidad. 
+    BCF		TXSTA, 4		;Modo Asincronico. 
+    BSF		TXSTA,  5		;Bit de transmicion activo. 
+    BCF		TXSTA,  6		;Transmicion de 8 bits. 
     ;-------------------Configuracion de Interrupcion--------------------------------------
     BSF		INTCON, GIE		;Se habilitaron la interrupciones globales,
     BSF		INTCON, PEIE		;perifericas,
     BSF		PIE1, ADIE		;y la interrupciones por el A/D.
-    BCF		STATUS, 5		;-------------------Banco 0------------------
-    CLRF		PORTA			;Se limpian los puertos.
-    CLRF		PORTB
-    CLRF		PORTD
-    CLRF		DISPLAY_V		;Se limpian las variables.
-    CLRF		NIBB_H
-    CLRF		NIBB_L    
-    CLRF		FLAGS
-EJE_X:
     MOVLW	B'01100001'		;Fosc/8, ANS8 & conversion activada.
     MOVWF	ADCON0
-    BCF		PIR1, ADIF		;Se apaga la bandera del A/D.
+    BCF		STATUS, 5		;-------------------Banco 0------------------
+  ;-----------------Configuracion de Recepcion/transmision-------------------------------
+    BSF		RCSTA, SPEN		;Habilitar puerto serial. 
+    BSF		RCSTA, CREN		;Habilitar recepcion continua. 	
+    BCF		RCSTA, RX9		;Recepcion de 8 bits. 	
+   ;----------------------------Se limpian los puertos-------------------------------------
+    CLRF		PORTA			
+    CLRF		PORTB
+    CLRF		PORTD
+    ;----------------------------Se limpian las variables------------------------------------
+    CLRF		DISPLAY_V		
+    CLRF		NIBB_H
+    CLRF		NIBB_L    
+    CLRF		FLAGS    
+EJE_X:
+    CLRF		LED
+    ;BCF		PIR1, ADIF		;Se apaga la bandera del A/D.
     CALL		DELAY_4US
     BSF		ADCON0, GO
+    BTFSC	ADCON0, GO
+    GOTO		$-1
+    MOVFW	ADRESH
+    MOVWF	DISPLAY_V
     CALL		SEPARAR_NIBBLES
     CALL		DISPLAY
 EJE_Y: 
+    BSF		LED, VALOR_CERO
     MOVLW	B'01101001'		;Fosc/8, ANS10 & conversion activada.
     MOVWF	ADCON0
     BCF		PIR1, ADIF		;Se apaga la bandera del A/D.
     CALL		DELAY_4US
     BSF		ADCON0, GO
+   BTFSC		ADCON0, GO
+    GOTO		$-1
+    MOVFW	ADRESH
+    MOVWF	DISPLAY_V
     CALL		SEPARAR_NIBBLES
     CALL		DISPLAY
-    GOTO		EJE_Y
+FIN:    
+    CALL		TOGGLES  
+    GOTO		EJE_X
     ;configurar una especie de menu donde se use algun bit para mantener la seleccion del eje 
     ; los nibbles son los que tienen que ser especificos. restringir el aumento y la disminucion 
     ;del tercer display.
@@ -134,55 +168,67 @@ DISPLAY
    CLRF		PORTA			    ;valores de las variables.
     MOVF		FLAGS,W		    ;Dependiendo del actual valor de banderas
     ADDWF	PCL,F			    ;se selecciona el diplays que va a mostrar su valor:
-    GOTO		DISPLAY_0		    ;Para los displays del eje x
+   GOTO		DISPLAY_0		    ;Para los displays del eje x
     GOTO		DISPLAY_1		    ;	    ||
     GOTO		DISPLAY_2		    ;	    ||
-    GOTO		DISPLAY_3		    ;Para los displays del eje y
-    GOTO		DISPLAY_4		    ;	    ||
-    GOTO		DISPLAY_5		    ;	    ||
+    ;GOTO		DISPLAY_3		    ;Para los displays del eje y
+ ;  GOTO		DISPLAY_4		    ;	    ||
+    ;GOTO		DISPLAY_5		    ;	    ||
 DISPLAY_0:
    MOVF		NIBB_H, W		
    CALL		TABLA			
-   MOVWF	PORTD			
-   BSF		PORTA,  0	
+   MOVWF	PORTD	
+   BTFSC		LED, VALOR_CERO
+   GOTO		$+3
+   BSF		PORTA,  0
+   GOTO		END_DIS
+   BSF		PORTA,  3	
    GOTO		END_DIS
 DISPLAY_1:   
    MOVF		NIBB_L, W		
    CALL		TABLA			
-   MOVWF	PORTD			
-   BSF		PORTA,  1		
+   MOVWF	PORTD
+  BTFSC		LED, VALOR_CERO
+   GOTO		$+3
+   BSF		PORTA,  1
+   GOTO		END_DIS
+   BSF		PORTA,  4	
    GOTO		END_DIS
 DISPLAY_2:
    MOVF		NIBB_H, W		
-   CALL		TABLA			
-   MOVWF	PORTD			
-   BSF		PORTA,  2	
+   CALL		TABLA
+   MOVWF	PORTD
+   BTFSC    	LED, VALOR_CERO 
+   GOTO		$+3
+   BSF		PORTA,  2
    GOTO		END_DIS
-DISPLAY_3:   
-   MOVF		NIBB_L, W		
-   CALL		TABLA			
-   MOVWF	PORTD			
-   BSF		PORTA,  3		
+   BSF		PORTA,  5	
    GOTO		END_DIS
-DISPLAY_4:
-   MOVF		NIBB_H, W		
-   CALL		TABLA			
-   MOVWF	PORTD			
-   BSF		PORTA,  4	
-   GOTO		END_DIS
-DISPLAY_5:   
-   MOVF		NIBB_L, W		
-   CALL		TABLA			
-   MOVWF	PORTD			
-   BSF		PORTA,  5		
-   GOTO		END_DIS
+;DISPLAY_3:   
+ ;  MOVF		NIBB_L, W		
+  ; CALL		TABLA			
+  ; MOVWF	PORTD			
+   ;BSF		PORTA,  3		
+   ;GOTO		END_DIS
+;DISPLAY_4:
+  ; MOVF		NIBB_H, W		
+ ;  CALL		TABLA			
+  ; MOVWF	PORTD			
+  ; BSF		PORTA,  4	
+  ; GOTO		END_DIS
+;DISPLAY_5:   
+  ; MOVF		NIBB_L, W		
+   ;CALL		TABLA
+ ;  MOVLW	VALOR_CERO
+ ;  MOVWF	PORTD			
+;   BSF		PORTA,  5		
+   ;GOTO		END_DIS
 END_DIS:
-  CALL		TOGGLES  
   RETURN  
   
 TOGGLES:				;Se incrementa el valor de banderas cada
     INCF		FLAGS,F		;vez que que se llama a la funcion.
-    MOVLW	.6			;Debido a que hay 6 displays, se resetea
+    MOVLW	.3			;Debido a que hay 6 displays, se resetea
     SUBWF	FLAGS,W		;la variable cada vez que esta tiene un valor
     BTFSC	STATUS, Z		;de 6.
     CLRF		FLAGS
--- Proyecto_2.X/build/default/production/Proyecto2.o.d
+++ Proyecto_2.X/build/default/production/Proyecto2.o.d
@@ -1,3 +1,3 @@
 build/default/production/Proyecto2.o:  \
-	C:/Users/Daniel/Documents/GitHub/Proyecto\ 2_Etch\ a\ Sketch/Proyecto_2.X/Proyecto2.asm  \
+	C:/Users/Daniel/Documents/GitHub/Proyecto2_Etch_a_Sketch/Proyecto_2.X/Proyecto2.asm  \
 
